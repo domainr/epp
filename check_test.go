d65235c28f53821f9e93213e3792be03b4a67dc1
Randy Reddig <ydnar@shaderlab.com>
2015-06-07 18:01:17 +0200

    epp: tests for Conn.CheckDomain

---
+++ check_test.go
@@ -0,0 +1,29 @@
+package epp
+
+import (
+	"testing"
+
+	"github.com/nbio/st"
+)
+
+func TestConnCheck(t *testing.T) {
+	c := login(t)
+
+	dc, err := c.CheckDomain("google.com")
+	st.Expect(t, err, nil)
+	st.Reject(t, dc, nil)
+	st.Expect(t, len(dc.Results), 1)
+	st.Expect(t, dc.Results[0].Domain.Domain, "google.com")
+	st.Expect(t, dc.Results[0].Domain.IsAvailable, false)
+
+	dc, err = c.CheckDomain("dmnr-test-x759824vim-i2.com")
+	st.Expect(t, err, nil)
+	st.Reject(t, dc, nil)
+	st.Expect(t, len(dc.Results), 1)
+	st.Expect(t, dc.Results[0].Domain.Domain, "dmnr-test-x759824vim-i2.com")
+	st.Expect(t, dc.Results[0].Domain.IsAvailable, true)
+
+	dc, err = c.CheckDomain("--dmnr-test--.com")
+	st.Reject(t, err, nil)
+	st.Expect(t, dc, (*DomainCheck)(nil))
+}
