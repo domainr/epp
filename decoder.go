00656bfad41c618c4576b073548f9b915d7f13ce
Randy Reddig <ydnar@shaderlab.com>
2015-06-28 16:52:23 -0700

    override decoder.Token method to maintain internal stack

--- decoder.go
+++ decoder.go
@@ -2,6 +2,7 @@ package epp
 
 import (
 	"encoding/xml"
+	"fmt"
 	"io"
 )
 
@@ -14,19 +15,21 @@ var xmlHeader = []byte(xml.Header)
 type decoder struct {
 	xml.Decoder
 	saved xml.Decoder
+	stack []xml.StartElement
 }
 
 // newDecoder returns an initialized decoder.
 // The initial state of the xml.Decoder is copied to saved.
 func newDecoder(r io.Reader) decoder {
 	d := xml.NewDecoder(r)
-	return decoder{*d, *d}
+	return decoder{Decoder: *d, saved: *d}
 }
 
 // reset restores the original state of the underlying
 // xml.Decoder (pos 1, line 1, stack, etc.).
 func (d *decoder) reset() {
 	d.Decoder = d.saved
+	d.stack = d.stack[:0]
 }
 
 // decode decodes an EPP XML message into msg,
@@ -40,4 +43,21 @@ func (d *decoder) decode(msg *message) error {
 		return err
 	}
 	return msg.error()
+}
+
+// Token returns an xml.Token from its internal xml.Decoder or an error.
+// It maintains a stack of xml.StartElements.
+func (d *decoder) Token() (xml.Token, error) {
+	t, err := d.Decoder.Token()
+	switch node := t.(type) {
+	case xml.StartElement:
+		d.stack = append(d.stack, node)
+	case xml.EndElement:
+		n := len(d.stack)
+		if n == 0 || node.Name != d.stack[n-1].Name {
+			return t, fmt.Errorf("unbalanced end tag: %s", node.Name)
+		}
+		d.stack = d.stack[:n-1]
+	}
+	return t, err
 }
