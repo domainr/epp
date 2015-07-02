85830caa6a0f780995d0cc8b430ddc78267d34c7
Randy Reddig <ydnar@shaderlab.com>
2015-07-02 15:28:44 -0700

    Spike on recursive/nested response decoding.

---
+++ response.go
@@ -0,0 +1,84 @@
+package epp
+
+import "encoding/xml"
+
+type response_ struct {
+	result  Result
+	resData resData
+}
+
+func (d *Decoder) decodeResponse(res *response_) error {
+	*res = response_{}
+	return d.DecodeWith(func(t xml.Token) error {
+		var err error
+		switch node := t.(type) {
+		case xml.StartElement:
+			switch node.Name.Local {
+			case "result":
+				err = d.decodeResult(&res.result)
+			case "resData":
+				err = d.decodeResData(&res.resData)
+			}
+		}
+		return err
+	})
+}
+
+type resData struct {
+	domainChkData domainChkData
+}
+
+func (d *Decoder) decodeResData(rd *resData) error {
+	*rd = resData{}
+	return d.DecodeWith(func(t xml.Token) error {
+		var err error
+		switch node := t.(type) {
+		case xml.StartElement:
+			switch node.Name.Local {
+			case "domainChkData":
+				err = d.decodeDomainChkData(&rd.domainChkData)
+			}
+		}
+		return err
+	})
+}
+
+type domainChkData struct {
+	cds []domainCD
+}
+
+func (d *Decoder) decodeDomainChkData(v *domainChkData) error {
+	*v = domainChkData{}
+	return d.DecodeWith(func(t xml.Token) error {
+		var err error
+		switch node := t.(type) {
+		case xml.StartElement:
+			switch node.Name.Local {
+			case "cd":
+				v.cds = append(v.cds, domainCD{})
+				err = d.decodeDomainCD(&v.cds[len(v.cds)-1])
+			}
+		}
+		return err
+	})
+}
+
+type domainCD struct {
+	name  string
+	avail bool
+}
+
+func (d *Decoder) decodeDomainCD(v *domainCD) error {
+	*v = domainCD{}
+	return d.DecodeWith(func(t xml.Token) error {
+		var err error
+		// switch node := t.(type) {
+		// case xml.StartElement:
+		// 	switch node.Name.Local {
+		// 	case "cd":
+		// 		err = d.decodeDomainCD(&v.cds[len(v.cds)-1])
+		// 	}
+		// }
+		return err
+	})
+}
