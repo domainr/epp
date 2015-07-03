c1710a52aa15d9cf1c58b69b19765445d278a4b9
Randy Reddig <ydnar@shaderlab.com>
2015-07-03 13:57:44 -0700

    Created deleteRange and deleteBufferRange funcs to delete ignored blocks of XML.

--- xml.go
+++ xml.go
@@ -1,6 +1,9 @@
 package epp
 
-import "encoding/xml"
+import (
+	"bytes"
+	"encoding/xml"
+)
 
 const (
 	startEPP = `<epp xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="urn:ietf:params:xml:ns:epp-1.0 epp-1.0.xsd" xmlns="urn:ietf:params:xml:ns:epp-1.0">`
@@ -14,3 +17,23 @@ var (
 	xmlCommandPrefix = []byte(xml.Header + startEPP + `<command>`)
 	xmlCommandSuffix = []byte(`</command>` + endEPP)
 )
+
+func deleteRange(s, pfx, sfx []byte) []byte {
+	start := bytes.Index(s, pfx)
+	if start < 0 {
+		return s
+	}
+	end := bytes.Index(s[start+len(pfx):], sfx)
+	if end < 0 {
+		return s
+	}
+	end += start + len(pfx) + len(sfx)
+	size := len(s) - (end - start)
+	copy(s[start:size], s[end:])
+	return s[:size]
+}
+
+func deleteBufferRange(buf *bytes.Buffer, pfx, sfx []byte) {
+	v := deleteRange(buf.Bytes(), pfx, sfx)
+	buf.Truncate(len(v))
+}
--- xml_test.go
+++ xml_test.go
@@ -1,6 +1,7 @@
 package epp
 
 import (
+	"bytes"
 	"encoding/xml"
 	"testing"
 
@@ -19,4 +20,22 @@ func TestMarshalOmitEmpty(t *testing.T) {
 	x, err := xml.Marshal(&v)
 	st.Expect(t, err, nil)
 	st.Expect(t, string(x), `<hello><foo></foo><bar><baz></baz></bar></hello>`)
+}
+
+func TestDeleteRange(t *testing.T) {
+	v := deleteRange([]byte(`<foo><bar><baz></baz></bar></foo>`), []byte(`<baz`), []byte(`</baz>`))
+	st.Expect(t, string(v), `<foo><bar></bar></foo>`)
+
+	v = deleteRange([]byte(`<foo><bar><baz></baz></bar></foo>`), []byte(`</bar>`), []byte(`o>`))
+	st.Expect(t, string(v), `<foo><bar><baz></baz>`)
+}
+
+func TestDeleteBufferRange(t *testing.T) {
+	buf := bytes.NewBufferString(`<foo><bar><baz></baz></bar></foo>`)
+	deleteBufferRange(buf, []byte(`<baz`), []byte(`</baz>`))
+	st.Expect(t, buf.String(), `<foo><bar></bar></foo>`)
+
+	buf = bytes.NewBufferString(`<foo><bar><baz></baz></bar></foo>`)
+	deleteBufferRange(buf, []byte(`</bar>`), []byte(`o>`))
+	st.Expect(t, buf.String(), `<foo><bar><baz></baz>`)
 }
