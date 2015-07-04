34bb9ef372c5d9fef34c184ecb4a3210792642b1
Randy Reddig <ydnar@shaderlab.com>
2015-07-04 11:00:48 -0700

    Remove XML comment.

--- check.go
+++ check.go
@@ -1,25 +1,5 @@
 package epp
 
-// <epp xmlns="urn:ietf:params:xml:ns:epp-1.0">
-//   <command>
-//     <check>
-//     <domain:check xmlns:domain="urn:ietf:params:xml:ns:domain-1.0">
-//       <domain:name>dmnr-test-1234.com</domain:name>
-//     </domain:check>
-//     </check>
-//     <clTRID>ABC-12345</clTRID>
-//   </command>
-// </epp>
-
-// <resData>
-//  <domain:chkData xmlns:domain="urn:ietf:params:xml:ns:domain-1.0" xsi:schemaLocation="urn:ietf:params:xml:ns:domain-1.0 domain-1.0.xsd">
-//   <domain:cd>
-//    <domain:name avail="0">ace.pizza</domain:name>
-//    <domain:reason>Premium Domain Name</domain:reason>
-//   </domain:cd>
-//  </domain:chkData>
-// </resData>
-
 // CheckDomain queries the EPP server for the availability status of one or more domains.
 func (c *Conn) CheckDomain(domains ...string) (*DomainCheckResponse, error) {
 	req := message{
