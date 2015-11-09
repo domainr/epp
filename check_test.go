ec86433d49d982f19ce677e5c0c7e72fc654cc51
wbond <will@wbond.net>
2015-11-09 10:50:13 -0500

    Add detection of <fee:name premium=true> for urn:ietf:params:xml:ns:fee-0.5 extension

--- check_test.go
+++ check_test.go
@@ -147,6 +147,53 @@ func TestScanCheckDomainResponseWithFee(t *testing.T) {
 	st.Expect(t, dcr.Charges[0].CategoryName, "Premium Registration Fee")
 }
 
+func TestScanCheckDomainResponseWithPremiumAttribute(t *testing.T) {
+	x := `<?xml version="1.0" encoding="UTF-8"?>
+<epp xmlns="urn:ietf:params:xml:ns:epp-1.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="urn:ietf:params:xml:ns:epp-1.0 epp-1.0.xsd">
+	<response>
+		<result code="1000">
+			<msg>Command completed successfully</msg>
+		</result>
+		<resData>
+			<domain:chkData xmlns:domain="urn:ietf:params:xml:ns:domain-1.0">
+				<domain:cd>
+					<domain:name avail="1">zero.work</domain:name>
+				</domain:cd>
+			</domain:chkData>
+		</resData>
+		<extension>
+			<fee:chkData xmlns:fee="urn:ietf:params:xml:ns:fee-0.5">
+				<fee:cd>
+					<fee:name premium="true">zero.work</fee:name>
+					<fee:currency>USD</fee:currency>
+					<fee:command>create</fee:command>
+					<fee:period unit="y">1</fee:period>
+					<fee:fee description="Registration Fee">500.000</fee:fee>
+				</fee:cd>
+			</fee:chkData>
+		</extension>
+		<trID>
+			<svTRID>14470834306141</svTRID>
+		</trID>
+	</response>
+</epp>`
+
+	var res response_
+	dcr := &res.DomainCheckResponse
+
+	d := decoder(x)
+	err := IgnoreEOF(scanResponse.Scan(d, &res))
+	st.Expect(t, err, nil)
+	st.Expect(t, len(dcr.Checks), 1)
+	st.Expect(t, dcr.Checks[0].Domain, "zero.work")
+	st.Expect(t, dcr.Checks[0].Available, true)
+	st.Expect(t, dcr.Checks[0].Reason, "")
+	st.Expect(t, len(dcr.Charges), 1)
+	st.Expect(t, dcr.Charges[0].Domain, "zero.work")
+	st.Expect(t, dcr.Charges[0].Category, "premium")
+	st.Expect(t, dcr.Charges[0].CategoryName, "Registration Fee")
+}
+
 func BenchmarkEncodeDomainCheck(b *testing.B) {
 	var buf bytes.Buffer
 	domains := []string{"hello.com"}
