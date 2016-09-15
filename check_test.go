688851fa2f7452d51f185423a4823550adfb0a42
Cameron Walters <cameron.walters@gmail.com>
2016-09-15 14:00:25 -0700

    Add a test for multiple charge:sets

--- check_test.go
+++ check_test.go
@@ -121,6 +121,66 @@ func TestScanCheckDomainResponseWithCharge(t *testing.T) {
 	st.Expect(t, dcr.Charges[0].Category, "premium")
 	st.Expect(t, dcr.Charges[0].CategoryName, "BBB+")
 }
+func TestScanCheckDomainResponseWithMultipleChargeSets(t *testing.T) {
+	x := `<?xml version="1.0" encoding="utf-8"?>
+<epp xmlns="urn:ietf:params:xml:ns:epp-1.0">
+	<response>
+		<result code="1000">
+			<msg>Command completed successfully</msg>
+		</result>
+		<resData>
+			<domain:chkData xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="urn:ietf:params:xml:ns:domain-1.0 domain-1.0.xsd" xmlns:domain="urn:ietf:params:xml:ns:domain-1.0">
+				<domain:cd>
+					<domain:name avail="1">good.memorial</domain:name>
+					<domain:reason>premium name</domain:reason>
+				</domain:cd>
+			</domain:chkData>
+		</resData>
+		<extension>
+			<charge:chkData xmlns:charge="http://www.unitedtld.com/epp/charge-1.0">
+				<charge:cd>
+					<charge:name>good.memorial</charge:name>
+					<charge:set>
+						<charge:category name="BBB+">premium</charge:category>
+						<charge:type>price</charge:type>
+						<charge:amount command="create">100.00</charge:amount>
+						<charge:amount command="renew">100.00</charge:amount>
+						<charge:amount command="transfer">100.00</charge:amount>
+						<charge:amount command="update" name="restore">50.00</charge:amount>
+					</charge:set>
+					<charge:set>
+						<charge:category>earlyAccess</charge:category>
+						<charge:type>fee</charge:type>
+						<charge:amount command="create">2500.00</charge:amount>
+					</charge:set>
+				</charge:cd>
+			</charge:chkData>
+		</extension>
+		<trID>
+			<clTRID>0000000000000002</clTRID>
+			<svTRID>83fa5767-5624-4be5-9e54-0b3a52f9de5b:1</svTRID>
+		</trID>
+	</response>
+</epp>`
+
+	var res response_
+	dcr := &res.DomainCheckResponse
+
+	d := decoder(x)
+	err := IgnoreEOF(scanResponse.Scan(d, &res))
+	st.Expect(t, err, nil)
+	st.Expect(t, len(dcr.Checks), 1)
+	st.Expect(t, dcr.Checks[0].Domain, "good.memorial")
+	st.Expect(t, dcr.Checks[0].Available, true)
+	st.Expect(t, dcr.Checks[0].Reason, "premium name")
+	st.Expect(t, len(dcr.Charges), 2)
+	st.Expect(t, dcr.Charges[0].Domain, "good.memorial")
+	st.Expect(t, dcr.Charges[0].Category, "premium")
+	st.Expect(t, dcr.Charges[0].CategoryName, "BBB+")
+	st.Expect(t, dcr.Charges[1].Domain, "good.memorial")
+	st.Expect(t, dcr.Charges[1].Category, "earlyAccess")
+	st.Expect(t, dcr.Charges[1].CategoryName, "")
+}
 
 func TestScanCheckDomainResponseWithFee05(t *testing.T) {
 	x := `<?xml version="1.0" encoding="utf-8"?>
