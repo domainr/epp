54ea7551bd14cb524f5270754fcc3bd1e640c86b
Cameron Walters <cameron.walters@gmail.com>
2016-09-22 12:40:11 -0700

    Pretty-print XML when debugging

--- logging.go
+++ logging.go
@@ -1,6 +1,8 @@
 package epp
 
 import (
+	"bytes"
+	"encoding/xml"
 	"fmt"
 	"io"
 )
@@ -8,9 +10,38 @@ import (
 // DebugLogger is an io.Writer. Set to enable logging of EPP message XML.
 var DebugLogger io.Writer
 
-func logXML(pfx string, xml []byte) {
+func logXML(pfx string, p []byte) {
 	if DebugLogger == nil {
 		return
 	}
-	fmt.Printf("%s\n%s\n\n", pfx, string(xml))
+
+	var b bytes.Buffer
+	enc := xml.NewEncoder(&b)
+	enc.Indent("", "\t")
+
+	dec := xml.NewDecoder(bytes.NewReader(p))
+	var t xml.Token
+	var err error
+	for {
+		t, err = dec.RawToken()
+		if err == io.EOF {
+			err = enc.Flush()
+			break
+		}
+		if err != nil {
+			break
+		}
+		err = enc.EncodeToken(t)
+		if err != nil {
+			break
+		}
+	}
+	if err != nil {
+		fmt.Fprintf(DebugLogger, "Indentation error. Raw XML: %s\n%s\n\n", pfx, string(p))
+		return
+	}
+
+	fmt.Fprintf(DebugLogger, "%s (pretty-printed)\n", pfx)
+	io.Copy(DebugLogger, &b)
+	fmt.Fprint(DebugLogger, "\n\n")
 }
