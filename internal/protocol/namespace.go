0becb0c38df3386f17badec1f4755cbf1f2c4561
Randy Reddig <randy@domainr.com>
2021-09-22 12:40:45 -0700

    internal/protocol: improve baseNamespace

--- internal/protocol/namespace.go
+++ internal/protocol/namespace.go
@@ -2,21 +2,33 @@ package protocol
 
 import (
 	"encoding"
+	"encoding/xml"
 
 	"github.com/domainr/epp/ns"
 )
 
-type eppNamespace struct{}
+type baseNamespace struct{}
 
-var _ encoding.TextMarshaler = eppNamespace{}
+var (
+	_ encoding.TextMarshaler = baseNamespace{}
+	_ xml.UnmarshalerAttr    = baseNamespace{}
+)
+
+func (baseNamespace) MarshalText() ([]byte, error) {
+	return nil, nil
+}
+
+func (baseNamespace) UnmarshalXMLAttr(attr xml.Attr) error {
+	return nil
+}
+
+type eppNamespace struct{ baseNamespace }
 
 func (eppNamespace) MarshalText() ([]byte, error) {
 	return unsafeBytes(ns.EPP), nil
 }
 
-type domainNamespace struct{}
-
-var _ encoding.TextMarshaler = domainNamespace{}
+type domainNamespace struct{ baseNamespace }
 
 func (domainNamespace) MarshalText() ([]byte, error) {
 	return unsafeBytes(ns.Domain), nil
