9d3d2bdf8dcfce8b6b8e1c3b8bbf83e8c466e2de
Randy Reddig <randy@domainr.com>
2021-11-29 11:30:34 -0800

    internal/schema/{domain,epp}: remove redundant namespace URIs

--- internal/schema/domain/domain.go
+++ internal/schema/domain/domain.go
@@ -2,5 +2,5 @@ package domain
 
 type Check struct {
 	XMLName struct{} `xml:"urn:ietf:params:xml:ns:domain-1.0 domain:check"`
-	Names   []string `xml:"urn:ietf:params:xml:ns:domain-1.0 domain:name,omitempty"`
+	Names   []string `xml:"domain:name,omitempty"`
 }
--- internal/schema/epp/epp.go
+++ internal/schema/epp/epp.go
@@ -28,25 +28,25 @@ type Hello struct {
 // Greeting represents an EPP server <greeting> message as defined in RFC 5730.
 type Greeting struct {
 	XMLName     struct{}   `xml:"urn:ietf:params:xml:ns:epp-1.0 greeting"`
-	ServerName  string     `xml:"urn:ietf:params:xml:ns:epp-1.0 svID,omitempty"`
-	ServerDate  *date.Time `xml:"urn:ietf:params:xml:ns:epp-1.0 svDate,omitempty"`
+	ServerName  string     `xml:"svID,omitempty"`
+	ServerDate  *date.Time `xml:"svDate,omitempty"`
 	ServiceMenu *ServiceMenu
 }
 
 // ServiceMenu represents an EPP <svcMenu> element as defined in RFC 5730.
 type ServiceMenu struct {
 	XMLName    struct{} `xml:"urn:ietf:params:xml:ns:epp-1.0 svcMenu"`
-	Versions   []string `xml:"urn:ietf:params:xml:ns:epp-1.0 version,omitempty"`
-	Languages  []string `xml:"urn:ietf:params:xml:ns:epp-1.0 lang,omitempty"`
-	Objects    []string `xml:"urn:ietf:params:xml:ns:epp-1.0 objURI,omitempty"`
-	Extensions []string `xml:"urn:ietf:params:xml:ns:epp-1.0 svcExtension>extURI,omitempty"`
+	Versions   []string `xml:"version,omitempty"`
+	Languages  []string `xml:"lang,omitempty"`
+	Objects    []string `xml:"objURI,omitempty"`
+	Extensions []string `xml:"svcExtension>extURI,omitempty"`
 }
 
 // Command represents an EPP client <command> message as defined in RFC 5730.
 type Command struct {
 	XMLName             struct{} `xml:"urn:ietf:params:xml:ns:epp-1.0 command"`
 	Check               *Check
-	ClientTransactionID string `xml:"urn:ietf:params:xml:ns:epp-1.0 clTRID,omitempty"`
+	ClientTransactionID string `xml:"clTRID,omitempty"`
 }
 
 // Check represents an EPP <check> command as defined in RFC 5730.
