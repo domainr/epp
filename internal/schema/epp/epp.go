69f5a93f57423d529adc801099efbe31fee7c5c1
Randy Reddig <randy@domainr.com>
2021-11-29 16:28:43 -0800

    internal/schema/epp: remove redundant namespace URI declarations

--- internal/schema/epp/epp.go
+++ internal/schema/epp/epp.go
@@ -11,52 +11,45 @@ type EPP struct {
 	XMLName struct{} `xml:"urn:ietf:params:xml:ns:epp-1.0 epp"`
 
 	// Hello represents a client <hello> message.
-	Hello *Hello
+	Hello *Hello `xml:"hello"`
 
 	// Greeting represents a server <greeting> message.
-	Greeting *Greeting
+	Greeting *Greeting `xml:"greeting"`
 
 	// Command represents a client <command> message.
-	Command *Command
+	Command *Command `xml:"command"`
 }
 
 // Hello represents an EPP client <hello> message as defined in RFC 5730.
-type Hello struct {
-	XMLName struct{} `xml:"urn:ietf:params:xml:ns:epp-1.0 hello"`
-}
+type Hello struct{}
 
 // Greeting represents an EPP server <greeting> message as defined in RFC 5730.
 type Greeting struct {
-	XMLName     struct{}   `xml:"urn:ietf:params:xml:ns:epp-1.0 greeting"`
-	ServerName  string     `xml:"svID,omitempty"`
-	ServerDate  *date.Time `xml:"svDate,omitempty"`
-	ServiceMenu *ServiceMenu
+	ServerName  string       `xml:"svID,omitempty"`
+	ServerDate  *date.Time   `xml:"svDate"`
+	ServiceMenu *ServiceMenu `xml:"svcMenu"`
 }
 
 // ServiceMenu represents an EPP <svcMenu> element as defined in RFC 5730.
 type ServiceMenu struct {
-	XMLName          struct{} `xml:"urn:ietf:params:xml:ns:epp-1.0 svcMenu"`
-	Versions         []string `xml:"version,omitempty"`
-	Languages        []string `xml:"lang,omitempty"`
-	Objects          []string `xml:"objURI,omitempty"`
-	ServiceExtension *ServiceExtension
+	Versions         []string          `xml:"version"`
+	Languages        []string          `xml:"lang"`
+	Objects          []string          `xml:"objURI"`
+	ServiceExtension *ServiceExtension `xml:"svcExtension"`
 }
 
 // ServiceExtension represents an EPP <svcExtension> element as defined in RFC 5730.
 type ServiceExtension struct {
-	XMLName    struct{} `xml:"urn:ietf:params:xml:ns:epp-1.0 svcExtension"`
-	Extensions []string `xml:"extURI,omitempty"`
+	Extensions []string `xml:"extURI"`
 }
 
 // Command represents an EPP client <command> message as defined in RFC 5730.
 type Command struct {
-	XMLName             struct{} `xml:"urn:ietf:params:xml:ns:epp-1.0 command"`
-	Check               *Check
+	Check               *Check `xml:"check"`
 	ClientTransactionID string `xml:"clTRID,omitempty"`
 }
 
 // Check represents an EPP <check> command as defined in RFC 5730.
 type Check struct {
-	XMLName     struct{} `xml:"urn:ietf:params:xml:ns:epp-1.0 check"`
 	DomainCheck *domain.Check
 }
