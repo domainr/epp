dfc7ecad6da98589e11c31d34aceabcf15a0507f
Randy Reddig <randy@domainr.com>
2021-11-29 16:43:14 -0800

    internal/schema/domain: doc comment

--- internal/schema/domain/domain.go
+++ internal/schema/domain/domain.go
@@ -1,5 +1,7 @@
 package domain
 
+// Check represents an EPP <domain:check> command.
+// See https://www.rfc-editor.org/rfc/rfc5730.html.
 type Check struct {
 	XMLName struct{} `xml:"urn:ietf:params:xml:ns:domain-1.0 domain:check"`
 	Names   []string `xml:"domain:name,omitempty"`
