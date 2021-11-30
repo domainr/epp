99e12ee551f66c751f52236b7f1d836e132d15c9
Randy Reddig <randy@domainr.com>
2021-11-30 11:51:00 -0800

    internal/schema/epp: remove redundant XMLName

--- internal/schema/epp/epp.go
+++ internal/schema/epp/epp.go
@@ -8,8 +8,6 @@ import (
 // EPP represents an <epp> message envelope as defined in RFC 5730.
 // See https://www.rfc-editor.org/rfc/rfc5730.html.
 type EPP struct {
-	XMLName struct{} `xml:"urn:ietf:params:xml:ns:epp-1.0 epp"`
-
 	// Hello represents a client <hello> message.
 	Hello *Hello `xml:"hello"`
 
