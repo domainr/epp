9b28de7db29216db49eb9fc1ac0425f35018768c
Randy Reddig <randy@domainr.com>
2021-11-30 16:45:56 -0800

    internal/schema/epp: use new option package

--- internal/schema/epp/greeting.go
+++ internal/schema/epp/greeting.go
@@ -1,10 +1,8 @@
 package epp
 
 import (
-	"reflect"
-	"strings"
-
 	"github.com/domainr/epp/internal/schema/date"
+	"github.com/domainr/epp/internal/schema/option"
 	"github.com/domainr/epp/internal/schema/raw"
 	"github.com/nbio/xml"
 )
@@ -87,29 +85,6 @@ func (a *Access) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
 	return nil
 }
 
-/*
-<dcp>
-	<access>
-		<all/>
-	</access>
-	<statement>
-		<purpose>
-			<admin/>
-			<other/>
-			<prov/>
-		</purpose>
-		<recipient>
-			<ours/>
-			<public/>
-			<unrelated/>
-		</recipient>
-		<retention>
-			<indefinite/>
-		</retention>
-	</statement>
-</dcp>
-*/
-
 // Statement describes an EPP server’s data collection purpose, receipient(s), and retention policy.
 type Statement struct {
 	Purpose   Purpose   `xml:"purpose"`
@@ -117,7 +92,7 @@ type Statement struct {
 }
 
 // Purpose represents an EPP server’s purpose for data collection.
-type Purpose int
+type Purpose uint64
 
 const (
 	PurposeAdmin = 1 << iota
@@ -126,84 +101,27 @@ const (
 	PurposeOther
 )
 
-type purposeTemplate struct {
-	Admin        *struct{} `xml:"admin"`
-	Contact      *struct{} `xml:"contact"`
-	Provisioning *struct{} `xml:"prov"`
-	Other        *struct{} `xml:"other"`
+var purposeNames = map[uint64]string{
+	PurposeAdmin:        "admin",
+	PurposeContact:      "contact",
+	PurposeProvisioning: "provisioning",
+	PurposeOther:        "other",
 }
 
+var purposeValues = option.Values(purposeNames)
+
 // MarshalXML implements xml.Marshaler.
-func (p Purpose) MarshalXML(e *xml.Encoder, start xml.StartElement) error {
-	var b strings.Builder
-	typ := reflect.TypeOf(purposeTemplate{})
-	for i := 0; i < typ.NumField(); i++ {
-		if p&(1<<i) == 0 {
-			continue
-		}
-		f := typ.Field(i)
-		if !f.IsExported() {
-			continue
-		}
-		tag := f.Tag.Get("xml")
-		if tag == "" || tag == "-" {
-			continue
-		}
-		tokens := strings.Split(tag, ",")
-		if tokens[0] == "" {
-			continue
-		}
-		b.WriteByte('<')
-		b.WriteString(tokens[0])
-		b.WriteString("/>")
-	}
-	// var s string
-	// if p&PurposeAdmin != 0 {
-	// 	s += "<admin/>"
-	// }
-	// if p&PurposeContact != 0 {
-	// 	s += "<contact/>"
-	// }
-	// if p&PurposeProvisioning != 0 {
-	// 	s += "<prov/>"
-	// }
-	// if p&PurposeOther != 0 {
-	// 	s += "<other/>"
-	// }
-	return e.EncodeElement(&raw.XML{Value: b.String()}, start)
+func (v Purpose) MarshalXML(e *xml.Encoder, start xml.StartElement) error {
+	return option.Encode(e, start, uint64(v), purposeNames)
 }
 
 // UnmarshalXML implements xml.Unmarshaler.
-func (p *Purpose) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
-	var v purposeTemplate
-	err := d.DecodeElement(&v, &start)
-	if err != nil {
-		return err
-	}
-	*p = 0
-	val := reflect.ValueOf(v)
-	for i := 0; i < val.NumField(); i++ {
-		if !val.Field(i).IsNil() {
-			*p |= 1 << i
-		}
-	}
-	// if v.Admin != nil {
-	// 	*p |= PurposeAdmin
-	// }
-	// if v.Contact != nil {
-	// 	*p |= PurposeContact
-	// }
-	// if v.Provisioning != nil {
-	// 	*p |= PurposeProvisioning
-	// }
-	// if v.Other != nil {
-	// 	*p |= PurposeOther
-	// }
-	return nil
+func (v *Purpose) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	return option.Decode((*uint64)(v), d, purposeValues)
 }
 
 // Recipient represents an EPP server’s purpose for data collection.
-type Recipient int
+type Recipient uint64
 
 const (
 	RecipientOther = 1 << iota
@@ -213,55 +131,22 @@ const (
 	RecipientUnrelated
 )
 
+var recipientNames = map[uint64]string{
+	RecipientOther:     "other",
+	RecipientOurs:      "ours",
+	RecipientPublic:    "public",
+	RecipientSame:      "same",
+	RecipientUnrelated: "unrelated",
+}
+
+var recipientValues = option.Values(recipientNames)
+
 // MarshalXML implements xml.Marshaler.
-func (r Recipient) MarshalXML(e *xml.Encoder, start xml.StartElement) error {
-	var s string
-	if r&RecipientOther != 0 {
-		s += "<other/>"
-	}
-	if r&RecipientOurs != 0 {
-		s += "<ours/>"
-	}
-	if r&RecipientPublic != 0 {
-		s += "<public/>"
-	}
-	if r&RecipientSame != 0 {
-		s += "<same/>"
-	}
-	if r&RecipientUnrelated != 0 {
-		s += "<unrelated/>"
-	}
-	return e.EncodeElement(&raw.XML{Value: s}, start)
+func (v Recipient) MarshalXML(e *xml.Encoder, start xml.StartElement) error {
+	return option.Encode(e, start, uint64(v), recipientNames)
 }
 
 // UnmarshalXML implements xml.Unmarshaler.
-func (r *Recipient) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
-	var v struct {
-		Other     *struct{} `xml:"other"`
-		Ours      *struct{} `xml:"ours"`
-		Public    *struct{} `xml:"public"`
-		Same      *struct{} `xml:"same"`
-		Unrelated *struct{} `xml:"unrelated"`
-	}
-	err := d.DecodeElement(&v, &start)
-	if err != nil {
-		return err
-	}
-	*r = 0
-	if v.Other != nil {
-		*r |= RecipientOther
-	}
-	if v.Ours != nil {
-		*r |= RecipientOurs
-	}
-	if v.Public != nil {
-		*r |= RecipientPublic
-	}
-	if v.Same != nil {
-		*r |= RecipientSame
-	}
-	if v.Unrelated != nil {
-		*r |= RecipientUnrelated
-	}
-	return nil
+func (v *Recipient) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	return option.Decode((*uint64)(v), d, recipientValues)
 }
