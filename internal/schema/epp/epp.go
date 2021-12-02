049f7e9771430691144f2846e15026b9a0706e53
Randy Reddig <randy@domainr.com>
2021-12-02 10:26:56 -0800

    internal/schema/epp: use ,selfclosing for <hello/> tag

--- internal/schema/epp/epp.go
+++ internal/schema/epp/epp.go
@@ -1,30 +1,16 @@
 package epp
 
-import (
-	"github.com/domainr/epp/internal/schema/raw"
-	"github.com/nbio/xml"
-)
-
 // EPP represents an <epp> message envelope as defined in RFC 5730.
 // See https://www.rfc-editor.org/rfc/rfc5730.html.
 type EPP struct {
+	XMLName struct{} `xml:"urn:ietf:params:xml:ns:epp-1.0 epp"`
+
 	// Hello represents a client <hello> message.
-	Hello *Hello `xml:"hello"`
+	Hello *Hello `xml:"hello,selfclosing"`
 
 	// Greeting represents a server <greeting> message.
 	Greeting *Greeting `xml:"greeting"`
 
 	// Command represents a client <command> message.
 	Command *Command `xml:"command"`
-}
-
-// MarshalXML implements xml.Marshaler, so a <hello/> message will be generated with a self-closing tag.
-func (epp *EPP) MarshalXML(e *xml.Encoder, start xml.StartElement) error {
-	start.Name.Space = "urn:ietf:params:xml:ns:epp-1.0"
-	start.Name.Local = "epp"
-	if epp.Hello != nil {
-		return e.EncodeElement(&raw.XML{Value: "<hello/>"}, start)
-	}
-	type T EPP
-	return e.EncodeElement((*T)(epp), start)
 }
