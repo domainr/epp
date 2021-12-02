d42435f4901fb784cd0923e9f60bb2ef38de52b9
Randy Reddig <randy@domainr.com>
2021-12-02 10:07:16 -0800

    internal/schema/test: add helper to encode with an explicit xml.Name

--- internal/schema/test/test.go
+++ internal/schema/test/test.go
@@ -1,13 +1,15 @@
 package test
 
 import (
+	"bytes"
 	"reflect"
 	"testing"
 
 	"github.com/nbio/xml"
 )
 
-// RoundTrip validates if xml.RoundTrip(v) produces want or wantErr (if set).
+// RoundTrip validates if v marshals to want or wantErr (if set),
+// and the resulting XML unmarshals to v.
 func RoundTrip(t *testing.T, v interface{}, want string, wantErr bool) {
 	x, err := xml.Marshal(v)
 	if (err != nil) != wantErr {
@@ -24,6 +26,40 @@ func RoundTrip(t *testing.T, v interface{}, want string, wantErr bool) {
 
 	i := reflect.New(reflect.TypeOf(v).Elem()).Interface()
 	err = xml.Unmarshal(x, i)
+	if err != nil {
+		t.Errorf("xml.Unmarshal() error = %v", err)
+		return
+	}
+	if !reflect.DeepEqual(v, v) {
+		t.Errorf("xml.Unmarshal()\nGot:  %#v\nWant: %#v", i, v)
+	}
+}
+
+// RoundTripName validates if v marshals to want or wantErr (if set),
+// and the resulting XML unmarshals to v. The outer XML tag will use name, if set.
+func RoundTripName(t *testing.T, name xml.Name, v interface{}, want string, wantErr bool) {
+	var err error
+	buf := &bytes.Buffer{}
+	enc := xml.NewEncoder(buf)
+	if name == (xml.Name{}) {
+		err = enc.Encode(v)
+	} else {
+		err = enc.EncodeElement(v, xml.StartElement{Name: name})
+	}
+	if (err != nil) != wantErr {
+		t.Errorf("XML encoding error = %v, wantErr %v", err, wantErr)
+		return
+	}
+	if buf.String() != want {
+		t.Errorf("XML encoding\nGot:  %v\nWant: %v", buf.String(), want)
+	}
+
+	if v == nil {
+		return
+	}
+
+	i := reflect.New(reflect.TypeOf(v).Elem()).Interface()
+	err = xml.Unmarshal(buf.Bytes(), i)
 	if err != nil {
 		t.Errorf("xml.Unmarshal() error = %v", err)
 		return
